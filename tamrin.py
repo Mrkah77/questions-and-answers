5fb33f8f77d4ab70c3580ae027c73aa391a448f0
Mohammad <125614615+Mrkah77@users.noreply.github.com>
2025-08-30 21:50:42 +0330

    Update tamrin.py
    کد تمرین دیکشنری در پایتون

--- tamrin.py
+++ tamrin.py
@@ -1,14 +1,6 @@
-یک دیکشنری به نام fruit_prices بسازید که شامل قیمت‌های میوه‌ها باشد. قیمت‌ها به شرح زیر است:
+کد تمرین برای دیکشنری و یکسری تغییرات در آن:
 
-apple: 1500
-banana: 1000
-orange: 1200
-
-سپس کدی بنویسید که قیمت banana را تغییر داده و به 1100 تغییر دهد و apple را به طور کلی حذف کند. در آخر این دیکشنری را چاپ کنید.
-
-'''
-fruit_prices = {'apple': 1500 , 'banana': 1000 ,'orange': 1200}
+'fruit_prices = {'apple': 1500 , 'banana': 1000 ,'orange': 1200}
 fruit_pric["banana"] = "1100"
 del fruit_prices["apple"]
-print(fruit_prices)
-'''
+print(fruit_prices)'
